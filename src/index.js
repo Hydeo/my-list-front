3c8144cc40aa0784814bde71771ab95a409be338
Hydeo <p.garciapelayo@gmail.com>
2018-05-07 15:16:53 +0100

    Redux setup

--- src/index.js
+++ src/index.js
@@ -4,5 +4,23 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {Provider} from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, {history} from './store'
+import App2 from './containers/app'
+
+ReactDOM.render(
+	<div>
+		<App />
+
+		<Provider store={store}>
+			<ConnectedRouter history={history}>
+				<div>
+					<App2/>
+				</div>
+			</ConnectedRouter>
+		</Provider>
+	</div>
+
+	, document.getElementById('root'));
 registerServiceWorker();
